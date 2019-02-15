ec48576c458283b009ef000183d3467ce18ee472
Kevin Wang <kwangsan@gmail.com>
2019-02-15 18:45:02 -0500

    Implement POST_MUTATION and CreateLink component - style Link component - add CreateLink to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,26 @@
-import React, { Component } from 'react'
-import LinkList from './LinkList'
+import React, { Component } from "react";
+import LinkList from "./LinkList";
+import CreateLink from "./CreateLink";
 
 class App extends Component {
   render() {
-    return <LinkList />
+    return (
+      <>
+        <fieldset style={{}}>
+          <legend>
+            <b>Create a Link</b>
+          </legend>
+          <CreateLink />
+        </fieldset>
+        <fieldset style={{ marginTop: "20px" }}>
+          <legend>
+            <b>Links</b>
+          </legend>
+          <LinkList />
+        </fieldset>
+      </>
+    );
   }
 }
 
-export default App
+export default App;
