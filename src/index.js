0dd8969b400eed7bd5d23685707a9ab4970b818c
Kevin Wang <kwangsan@gmail.com>
2019-02-15 16:34:34 -0500

    Add Link & LinkList components - implement in App.js - fix bug in serviceWorker import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
-import * as serviceWorker from './serviceWorker
+import * as serviceWorker from './serviceWorker'
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
