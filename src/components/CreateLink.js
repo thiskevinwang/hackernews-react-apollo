2b7ff87009d6408414bf40c0818ca02703d2f93f
Kevin Wang <kwangsan@gmail.com>
2019-02-15 21:46:01 -0500

    Rewrite CreateLink as function component w/ Hooks

--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Mutation } from "react-apollo";
 import gql from "graphql-tag";
 
@@ -13,43 +13,37 @@ const POST_MUTATION = gql`
   }
 `;
 
-class CreateLink extends Component {
-  state = {
+const CreateLink = () => {
+  const [state, setState] = useState({
     description: "",
     url: ""
-  };
+  });
 
-  render() {
-    /*
-      Object destructuring to avoid errors with "this"
-      keyword in Mutation's 'variables' prop.
-    */
-    const { description, url } = this.state;
+  const { description, url } = state;
 
-    return (
-      <div>
-        <div className="flex flex-column mt3">
-          <input
-            className="mb2"
-            value={description}
-            onChange={e => this.setState({ description: e.target.value })}
-            type="text"
-            placeholder="A description for the link"
-          />
-          <input
-            className="mb2"
-            value={url}
-            onChange={e => this.setState({ url: e.target.value })}
-            type="text"
-            placeholder="The URL for the link"
-          />
-        </div>
-        <Mutation mutation={POST_MUTATION} variables={{ description, url }}>
-          {postMutation => <button onClick={postMutation}>Submit</button>}
-        </Mutation>
+  return (
+    <div>
+      <div className="flex flex-column mt3">
+        <input
+          className="mb2"
+          value={description}
+          onChange={e => setState({ ...state, description: e.target.value })}
+          type="text"
+          placeholder="A description for the link"
+        />
+        <input
+          className="mb2"
+          value={url}
+          onChange={e => setState({ ...state, url: e.target.value })}
+          type="text"
+          placeholder="The URL for the link"
+        />
       </div>
-    );
-  }
-}
+      <Mutation mutation={POST_MUTATION} variables={{ description, url }}>
+        {postMutation => <button onClick={postMutation}>Submit</button>}
+      </Mutation>
+    </div>
+  );
+};
 
 export default CreateLink;
