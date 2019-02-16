70948c4f1531ff0934f4fdd2e13c354087bff89e
Kevin Wang <kwangsan@gmail.com>
2019-02-16 17:44:23 -0500

    Update LinkList with subscriptions

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -3,6 +3,54 @@ import Link from "./Link";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 
+const NEW_LINKS_SUBSCRIPTION = gql`
+  subscription {
+    newLink {
+      id
+      url
+      description
+      createdAt
+      postedBy {
+        id
+        name
+      }
+      votes {
+        id
+        user {
+          id
+        }
+      }
+    }
+  }
+`;
+
+const NEW_VOTES_SUBSCRIPTION = gql`
+  subscription {
+    newVote {
+      id
+      link {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`;
+
 export const FEED_QUERY = gql`
   {
     feed {
@@ -36,10 +84,36 @@ class LinkList extends Component {
     store.writeQuery({ query: FEED_QUERY, data });
   };
 
+  _subscribeToNewLinks = subscribeToMore => {
+    subscribeToMore({
+      document: NEW_LINKS_SUBSCRIPTION,
+      updateQuery: (prev, { subscriptionData }) => {
+        if (!subscriptionData.data) return prev;
+        const newLink = subscriptionData.data.newLink;
+        const exists = prev.feed.links.find(({ id }) => id === newLink.id);
+        if (exists) return prev;
+
+        return Object.assign({}, prev, {
+          feed: {
+            links: [newLink, ...prev.feed.links],
+            count: prev.feed.links.length + 1,
+            __typename: prev.feed.__typename
+          }
+        });
+      }
+    });
+  };
+
+  _subscribeToNewVotes = subscribeToMore => {
+    subscribeToMore({
+      document: NEW_VOTES_SUBSCRIPTION
+    });
+  };
+
   render() {
     return (
       <Query query={FEED_QUERY}>
-        {({ loading, error, data }) => {
+        {({ loading, error, data, subscribeToMore }) => {
           if (loading) {
             console.log(loading);
             return <div>Fetching</div>;
@@ -48,6 +122,11 @@ class LinkList extends Component {
             console.log(error);
             return <div>Error</div>;
           }
+
+          // Calling _subscribeToNewLinks with it respective subscribeToMore function you make sure that the component actually subscribes to the events.
+          // This call opens up a websocket connection to the subscription server.
+          this._subscribeToNewLinks(subscribeToMore);
+          this._subscribeToNewVotes(subscribeToMore);
 
           const linksToRender = data.feed.links;
           console.log(data);
