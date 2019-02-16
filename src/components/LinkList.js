1407ff400a207a3167be6e4dba86f55c89d81578
Kevin Wang <kwangsan@gmail.com>
2019-02-16 00:17:16 -0500

    Add _updateCacheAfterVote method to LinkList component - pass to Link's 'updateStoreAfterVote' props

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -27,6 +27,15 @@ const FEED_QUERY = gql`
 `;
 
 class LinkList extends Component {
+  _updateCacheAfterVote = (store, createVote, linkId) => {
+    const data = store.readQuery({ query: FEED_QUERY });
+
+    const votedLink = data.feed.links.find(link => link.id === linkId);
+    votedLink.votes = createVote.link.votes;
+
+    store.writeQuery({ query: FEED_QUERY, data });
+  };
+
   render() {
     return (
       <Query query={FEED_QUERY}>
@@ -46,7 +55,12 @@ class LinkList extends Component {
           return (
             <div>
               {linksToRender.map((link, index) => (
-                <Link key={link.id} link={link} index={index} />
+                <Link
+                  key={link.id}
+                  link={link}
+                  index={index}
+                  updateStoreAfterVote={this._updateCacheAfterVote}
+                />
               ))}
             </div>
           );
